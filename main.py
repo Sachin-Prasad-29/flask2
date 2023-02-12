663809b462b8fbc810481aa5570e827a5c5b04ab
Sachin kumar prasad <skpkorba9009@gmail.com>
2023-02-12 22:08:46 +0530

    fsdsdf

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
+from youtube_transcript_api import YouTubeTranscriptApi
 import os
 
 app = Flask(__name__)
@@ -8,6 +9,16 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+
+@app.route('/getcaption', methods=['POST'])
+def post_data():
+    try:
+        data = request.get_json()
+        srt = YouTubeTranscriptApi.get_transcript(data["id"])
+        # Do something with the received data
+        return srt
+    except Exception as e:
+        return 'Error: {}'.format(str(e)), 404
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
